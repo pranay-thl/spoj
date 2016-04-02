b0377469702b1e9dd1458009d1d864c5d9b03135
thehurtlocker-thl <pranay.pnd@gmail.com>
2016-04-02 12:34:09 +0530

    FCTRL

---
+++ FCTRL.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define LL long long int
+#define REP(i,n) for(int i=0;i<n;++i)
+#define REPP(i,a,b) for(int i=a;i<b;++i)
+#define TC int t;scanf("%d",&t);while (t-->0)
+#define INP(x) scanf("%d",&x)
+#define OUT(x) printf("%d\n",x)
+#define INPLL(x) scanf("%lld",&x)
+#define OUTLL(x) printf("%lld\n",x)
+#define INPS(x) scanf("%s",x)
+#define MEM(a,b) memset(a,b,sizeof(a))
+#define MP make_pair
+#define PB push_back
+#define PII pair<int,int>
+#define PLL pair<long long,long long>
+#define VPII vector<pair<int,int >
+#define F first
+#define S second
+#define MOD 1000000007
+#define mod 10000007
+using namespace std;
+int zero(int n)
+{
+    int rev=0;
+    while(n/5!=0)
+    {
+        rev=rev+n/5;
+        n=n/5;
+    }
+    return rev;
+}
+int main()
+{
+    int a;
+    TC
+    {
+        INP(a);
+        OUT(zero(a));
+ 
+    }
+    return 0;
+}
